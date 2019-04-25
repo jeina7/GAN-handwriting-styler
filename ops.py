c6fdeee6732addcfe9bbc29a1cfb61d401a65f84
jeina7 <jane1231001@gmail.com>
2019-04-25 23:28:09 +0900

    put relu first for each layers

--- ops.py
+++ ops.py
@@ -18,22 +18,22 @@ def batch_norm(c_out, momentum=0.1):
 
 def conv2d(c_in, c_out, k_size=5, stride=2, pad=2, dilation=2, bn=True, lrelu=True, leak=0.2):
     layers = []
+    if lrelu:
+        layers.append(nn.LeakyReLU(leak))
     layers.append(nn.Conv2d(c_in, c_out, k_size, stride, pad))
     if bn:
         layers.append(nn.BatchNorm2d(c_out))
-    if lrelu:
-        layers.append(nn.LeakyReLU(leak))
     return nn.Sequential(*layers)
 
 
-def deconv2d(c_in, c_out, k_size=3, stride=1, pad=1, dilation=1, bn=True, dropout=True, p=0.5):
+def deconv2d(c_in, c_out, k_size=3, stride=1, pad=1, dilation=1, bn=True, dropout=False, p=0.5):
     layers = []
+    layers.append(nn.LeakyReLU(0.2))
     layers.append(nn.ConvTranspose2d(c_in, c_out, k_size, stride, pad))
     if bn:
         layers.append(nn.BatchNorm2d(c_out))
     if dropout:
         layers.append(nn.Dropout(p))
-    layers.append(nn.LeakyReLU(0.1))
     return nn.Sequential(*layers)
 
 
