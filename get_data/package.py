fee2c14ad8361b6bc61ba050f1a8a7e9a4446633
Jeina <jane1231001@gmail.com>
2019-06-03 05:31:28 +0000

    Add: add with_charid option

--- get_data/package.py
+++ get_data/package.py
@@ -9,7 +9,7 @@ import pickle as pickle
 import random
 
 
-def pickle_examples(from_dir, train_path, val_path, train_val_split=0.2):
+def pickle_examples(from_dir, train_path, val_path, train_val_split=0.2, with_charid=False):
     """
     Compile a list of examples into pickled format, so during
     the training, all io will happen in memory
@@ -21,25 +21,45 @@ def pickle_examples(from_dir, train_path, val_path, train_val_split=0.2):
             c = 1
             val_count = 0
             train_count = 0
-            for p in paths:
-                c += 1
-                label = int(os.path.basename(p).split("_")[0])
-                with open(p, 'rb') as f:
-                    img_bytes = f.read()
-                    example = (label, img_bytes)
-                    r = random.random()
-                    if r < train_val_split:
-                        pickle.dump(example, fv)
-                        val_count += 1
-                        if val_count % 10000 == 0:
-                            print("%d imgs saved in val.obj" % val_count)
-                    else:
-                        pickle.dump(example, ft)
-                        train_count += 1
-                        if train_count % 10000 == 0:
-                            print("%d imgs saved in train.obj" % train_count)
-            print("%d imgs saved in val.obj, end" % val_count)
-            print("%d imgs saved in train.obj, end" % train_count)
+            if with_charid:
+                for p in paths:
+                    c += 1
+                    label = int(os.path.basename(p).split("_")[0])
+                    charid = int(os.path.basename(p).split("_")[1].split(".")[0])
+                    with open(p, 'rb') as f:
+                        img_bytes = f.read()
+                        example = (label, charid, img_bytes)
+                        r = random.random()
+                        if r < train_val_split:
+                            pickle.dump(example, fv)
+                            val_count += 1
+                            if val_count % 10000 == 0:
+                                print("%d imgs saved in val.obj" % val_count)
+                        else:
+                            pickle.dump(example, ft)
+                            train_count += 1
+                            if train_count % 10000 == 0:
+                                print("%d imgs saved in train.obj" % train_count)
+                print("%d imgs saved in val.obj, end" % val_count)
+                print("%d imgs saved in train.obj, end" % train_count)
+            else:
+                for p in paths:
+                    c += 1
+                    label = int(os.path.basename(p).split("_")[0])
+                    with open(p, 'rb') as f:
+                        img_bytes = f.read()
+                        example = (label, img_bytes)
+                        r = random.random()
+                        if r < train_val_split:
+                            pickle.dump(example, fv)
+                            val_count += 1
+                            if val_count % 10000 == 0:
+                                print("%d imgs saved in val.obj" % val_count)
+                        else:
+                            pickle.dump(example, ft)
+                            train_count += 1
+                            if train_count % 10000 == 0:
+                                print("%d imgs saved in train.obj" % train_count)
+                print("%d imgs saved in val.obj, end" % val_count)
+                print("%d imgs saved in train.obj, end" % train_count)
             return
-        
-        
